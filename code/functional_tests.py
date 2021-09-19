3bc5caac2730ca292ec9c8e59ee2f39f15b64ab0
Benjamin Kuhn <kuhnbt@gmail.com>
2021-09-19 10:04:30 -0400

    Add tests to populate the todo list with user input. Modify the home template to make a post request with user input.

--- code/functional_tests.py
+++ code/functional_tests.py
@@ -37,19 +37,28 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New todo item did not appear in table'
-        )
+        self.assertIn(
+            '1: Buy peacock feathers', [row.text for row in rows]
+            )
+            
 
         # There is still a text box inviting her to add another item
         # She enters 'use peacock feathers to make a fly'
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2. Use peacock feathers to make a fly', 
+                      [row.text for row in rows])
 
         # The site has generated a unique url to remember her list, and there is
         # some text to indicate this
+        self.fail('Finish the test!')
 
         # She visits the URL - her list is still there
 
